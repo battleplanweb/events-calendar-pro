50e3c239f5219caec3910224d311bf5ca02ff548
battleplanweb <70058676+battleplanweb@users.noreply.github.com>
2021-06-09 10:36:29 -0400

    5.7
    5.7

--- events-calendar-pro.php
+++ events-calendar-pro.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: The Events Calendar PRO
 Description: The Events Calendar PRO, a premium add-on to the open source The Events Calendar plugin (required), enables recurring events, custom attributes, venue pages, new widgets and a host of other premium features.
-Version: 5.6.0
+Version: 5.7.0
 GitHub Plugin URI: battleplanweb/events-calendar-pro
 Text Domain: tribe-events-calendar-pro
 License: GPLv2 or later
